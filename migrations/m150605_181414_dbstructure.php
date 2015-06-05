564d992edf63997d1561fc792f7f96f151efc9d2
Armano <sponsor15@gmail.com>
2015-06-05 20:20:43 +0200

    Clean

---
+++ migrations/m150605_181414_dbstructure.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150605_181414_dbstructure extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m150605_181414_dbstructure cannot be reverted.\n";
+
+        return false;
+    }
+
+}
